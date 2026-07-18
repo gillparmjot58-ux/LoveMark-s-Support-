9d9f8f6b3cf070d84a2b5db7f1a65d6a5e048481
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 00:37:52 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,23 @@
+module.exports = async (client, message) => {
+    if (message.author.bot) return;
+    if (!message.guild) return;
 
+    const prefix = ";";
+
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName);
+
+    if (!command) return;
+
+    try {
+        await command.execute(client, message, args);
+    } catch (error) {
+        console.error(error);
+
+        message.reply("❌ An error occurred while executing this command.");
+    }
+};
