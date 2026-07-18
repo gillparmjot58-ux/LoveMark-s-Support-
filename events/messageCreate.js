f2364a323485a48d57ec5be48bff72d3eea89f8f
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 01:54:44 +0530

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,11 +1,16 @@
 module.exports = async (client, message) => {
     if (message.author.bot) return;
+    if (!message.guild) return;
 
     const prefix = ";";
 
     if (!message.content.startsWith(prefix)) return;
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/\s+/);
+
     const commandName = args.shift().toLowerCase();
 
     const command = client.commands.get(commandName);
@@ -13,9 +18,9 @@ module.exports = async (client, message) => {
     if (!command) return;
 
     try {
-        await command.execute(message, args);
-    } catch (err) {
-        console.error(err);
+        await command.execute(client, message, args);
+    } catch (error) {
+        console.error(error);
         message.reply("❌ Error while executing command.");
     }
 };
