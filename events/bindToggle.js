24525d6292570677553fb5a23fe7b028e5c69744
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 03:06:36 +0530

    Update bindToggle.js

--- events/bindToggle.js
+++ events/bindToggle.js
@@ -1,1 +1,15 @@
+const Bind = require("../models/Bind");
+const Category = require("../models/Category");
 
+module.exports = {
+    name: "messageCreate",
+
+    async execute(client, message) {
+        if (message.author.bot) return;
+        if (!message.guild) return;
+        if (message.content.startsWith(";")) return;
+        if (!message.reference) return;
+
+        // Toggle logic yahin add hoga.
+    }
+};
