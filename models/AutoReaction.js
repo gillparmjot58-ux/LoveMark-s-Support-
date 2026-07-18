5355ba470d2a7797e6ff2a5bc26b15eaa3db1e5b
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 00:25:44 +0530

    Update AutoReaction.js

--- models/AutoReaction.js
+++ models/AutoReaction.js
@@ -1,1 +1,20 @@
+const { Schema, model } = require("mongoose");
 
+const autoReactionSchema = new Schema({
+    guildId: {
+        type: String,
+        required: true
+    },
+
+    userId: {
+        type: String,
+        required: true
+    },
+
+    emoji: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = model("AutoReaction", autoReactionSchema);
