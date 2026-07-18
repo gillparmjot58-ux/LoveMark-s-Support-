fe23d48faa4e53620f05d69ea6059356805fe361
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 00:24:01 +0530

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,1 +1,20 @@
+const { Schema, model } = require("mongoose");
 
+const categorySchema = new Schema({
+    guildId: {
+        type: String,
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    permissionRole: {
+        type: String,
+        default: null
+    }
+});
+
+module.exports = model("Category", categorySchema);
