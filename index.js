56d18c5d112b2010b5ca129bc7100a9d99e96ebb
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 00:22:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
+const connectMongo = require("./config/mongoose");
+
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.MessageContent
-  ]
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
-client.once("ready", () => {
-  console.log(`${client.user.tag} is online!`);
+client.once("ready", async () => {
+    await connectMongo();
+    console.log(`${client.user.tag} is online!`);
 });
 
 client.login(process.env.TOKEN);
