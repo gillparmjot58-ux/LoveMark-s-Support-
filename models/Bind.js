cda5bc2e860b0ebb38b2cb23797709115097e657
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 00:24:30 +0530

    Update Bind.js

--- models/Bind.js
+++ models/Bind.js
@@ -1,1 +1,25 @@
+const { Schema, model } = require("mongoose");
 
+const bindSchema = new Schema({
+    guildId: {
+        type: String,
+        required: true
+    },
+
+    category: {
+        type: String,
+        required: true
+    },
+
+    trigger: {
+        type: String,
+        required: true
+    },
+
+    roleId: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = model("Bind", bindSchema);
