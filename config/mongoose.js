02c0884592643b8abb4fd38f0378953ca0f9b445
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 00:21:18 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
 
+module.exports = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("✅ Connected to MongoDB");
+    } catch (error) {
+        console.error("❌ MongoDB Connection Failed:", error);
+        process.exit(1);
+    }
+};
