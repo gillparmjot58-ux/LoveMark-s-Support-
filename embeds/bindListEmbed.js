13838f34a635751e9a58171b6e20d7f3642c1573
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 13:47:48 +0530

    Update bindListEmbed.js

--- embeds/bindListEmbed.js
+++ embeds/bindListEmbed.js
@@ -1,1 +1,5 @@
-
+module.exports = (binds) => {
+    return binds.map((bind, index) =>
+        `\`${index + 1}\` **${bind.trigger}** → <@&${bind.roleId}> \`[${bind.category}]\``
+    ).join("\n");
+};
