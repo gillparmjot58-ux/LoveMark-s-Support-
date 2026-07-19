c771c1c11b707abb544038bfa20748a7fe14459b
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 16:24:07 +0530

    Create autoReactionRemoveEmbed.js

---
+++ embeds/autoReactionRemoveEmbed.js
@@ -0,0 +1,6 @@
+module.exports = (emoji, user) => {
+    return {
+        description: `<:emoji_159:1528161527344136323> Auto reaction ${emoji} removed for ${user}!`,
+        color: 0x5865f2
+    };
+};
