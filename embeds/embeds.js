df8c1f04f6e9f0e86bd14807719362382f6687df
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 12:48:08 +0530

    Update embeds.js

--- embeds/embeds.js
+++ embeds/embeds.js
@@ -1,1 +1,7 @@
+const { EmbedBuilder } = require("discord.js");
 
+module.exports = (description) => {
+    return new EmbedBuilder()
+        .setColor("#5865F2")
+        .setDescription(description);
+};
