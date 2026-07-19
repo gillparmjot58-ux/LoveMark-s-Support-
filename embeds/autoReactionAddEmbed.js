a1022ec871417f91de4872d223d17dba60df18e9
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 16:20:55 +0530

    Update and rename autoReactionEmbed.js to autoReactionAddEmbed.js

---
+++ embeds/autoReactionAddEmbed.js
@@ -0,0 +1,6 @@
+module.exports = (emoji, user, count) => {
+    return {
+        description: `<:emoji_159:1528161527344136323> Auto reaction ${emoji} added for ${user}! (${count}/10)`,
+        color: 0x5865f2
+    };
+};
--- embeds/autoReactionEmbed.js
+++
@@ -1,3 +0,0 @@
-module.exports = (user, emoji, channel) => {
-    return `<:emoji_159:1528161527344136323> ${user}: Added auto reaction \`${emoji}\` to ${channel}.`;
-};
