08e8454dc471499dc07610f69feff1abd61389f9
gillparmjot58-ux <gillparmjot58@gmail.com>
2026-07-19 21:36:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const express = require("express");
 const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
 const connectMongo = require("./config/mongoose");
 const commandHandler = require("./handlers/commandHandler");
 const eventHandler = require("./handlers/eventHandler");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot is running!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Web server running on port ${PORT}`);
+});
 
 const client = new Client({
     intents: [
